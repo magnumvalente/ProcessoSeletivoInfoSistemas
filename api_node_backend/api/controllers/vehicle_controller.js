feb0d50c6f6e5af546cf56695a47c627c63b5ca4
Magnum Oliveira <magnum.martins@dtidigital.com.br>
2020-06-10 18:18:51 -0300

    Gravação do arquivo e inclusão de regras

--- api_node_backend/api/controllers/vehicle_controller.js
+++ api_node_backend/api/controllers/vehicle_controller.js
@@ -1,4 +1,56 @@
 const { v4 : uuidv4 } = require('uuid');
+const fs = require('fs');
+
+function saveVehicleData(vehiclesData){
+    fs.writeFileSync('api/data/vehicle_data.json', JSON.stringify(vehiclesData));
+}
+
+function findVehicleIndex(vehiclesDataMock, req, res){
+
+    const { vehicleId } = req.params;
+    const vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.id === vehicleId);
+
+    if (vehicleIndex === -1){
+
+        res.status(404).json({
+            message: 'Veículo não encontrado.',
+            sucess: false
+        });
+    }
+    return vehicleIndex;
+}
+
+function validateSaveOrUpdateData(vehiclesDataMock, req, res){
+    
+    let vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.placa === req.body.placa);
+    if (vehicleIndex > -1){
+        res.status(409).json({
+            message: `Veículo com a placa '${req.body.placa}' já existe.`,
+            sucess: false
+        });
+        return false;
+    }
+
+    vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.chassi === req.body.chassi);
+    if (vehicleIndex > -1){
+        res.status(409).json({
+            message: `Veículo com o chassi '${req.body.chassi}' já existe.`,
+            sucess: false
+        });
+        return false;
+    }
+
+    vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.renavam === req.body.renavam);
+    if (vehicleIndex > -1){
+        res.status(409).json({
+            message: `Veículo com o renavam '${req.body.renavam}' já existe.`,
+            sucess: false
+        });
+        return false;
+    }
+
+    return true;
+}
 
 module.exports = app => {
 
@@ -10,66 +62,66 @@ module.exports = app => {
 
     controller.saveVehicle = (req, res) => {
 
-        vehiclesDataMock.data.push({
-            id: uuidv4(),
-            placa: req.body.placa,
-            chassi: req.body.chassi,
-            renavan: req.body.renavan,
-            modelo: req.body.modelo,
-            marca: req.body.marca,
-            ano: req.body.ano
-        });
+        if (validateSaveOrUpdateData(vehiclesDataMock, req, res)){
 
-        res.status(201).json(vehiclesDataMock);
-    }
-
-    controller.removeVehicle = (req, res) => {
-        const { vehicleId } = req.params;
-        const vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.id === vehicleId);
-
-        if (vehicleIndex === -1){
-            res.status(404).json({
-                message: 'Veículo não encontrado.',
-                sucess: false,
-                vehiclesDataDB: vehiclesDataMock
-            });
-        }else{
-            vehiclesDataMock.data.splice(vehicleIndex, 1);
-            
-            res.status(200).json({
-                sucess: true,
-                vehiclesDataDB: vehiclesDataMock
-            })
-        }
-    }
-
-    controller.updateVehicle = (req, res) => {
-        const { vehicleId } = req.params;
-        const vehicleIndex = vehiclesDataMock.data.findIndex(vehicle => vehicle.id === vehicleId);
-
-        if (vehicleIndex === -1){
-            res.status(404).json({
-                message: 'Veículo não encontrado.',
-                sucess: false,
-                vehiclesDataDB: vehiclesDataMock
-            });
-        }else{
-            const vehicleUpdated = {
-                id: vehicleId,
+            vehiclesDataMock.data.push({
+                id: uuidv4(),
                 placa: req.body.placa,
                 chassi: req.body.chassi,
                 renavan: req.body.renavan,
                 modelo: req.body.modelo,
                 marca: req.body.marca,
                 ano: req.body.ano
-            };
+            });
 
-            vehiclesDataMock.data.splice(vehicleIndex, 1, vehicleUpdated);
+            saveVehicleData(vehiclesData);
 
-            res.status(200).json({
-                sucess: true,
-                vehiclesDataDB: vehiclesDataMock
-            })
+            res.status(201).json(vehiclesDataMock);
+        }
+    }
+
+    controller.removeVehicle = (req, res) => {
+
+        const vehicleIndex = findVehicleIndex(vehiclesDataMock, req, res);
+
+        if (vehicleIndex > -1){
+
+            vehiclesDataMock.data.splice(vehicleIndex, 1);
+
+            saveVehicleData(vehiclesData);
+
+            res.status(200).json(vehiclesDataMock);
+        }
+    }
+
+    controller.updateVehicle = (req, res) => {
+
+        const vehicleIndex = findVehicleIndex(vehiclesDataMock, req, res);
+
+        if (vehicleIndex > -1){
+
+            const vehicleRemoved = vehiclesDataMock.data.splice(vehicleIndex, 1)[0];
+
+            if (validateSaveOrUpdateData(vehiclesDataMock, req, res)){
+
+                const { vehicleId } = req.params;
+                vehiclesDataMock.data.push({
+                    id: vehicleId,
+                    placa: req.body.placa,
+                    chassi: req.body.chassi,
+                    renavan: req.body.renavan,
+                    modelo: req.body.modelo,
+                    marca: req.body.marca,
+                    ano: req.body.ano
+                });
+            }else{
+
+                vehiclesDataMock.data.push(vehicleRemoved);
+            }
+
+            saveVehicleData(vehiclesData);
+
+            res.status(200).json(vehiclesDataMock);
         }
     }
 
--- api_node_backend/api/routes/vehicle_route.js
+++ api_node_backend/api/routes/vehicle_route.js
@@ -1,11 +1,11 @@
 module.exports = app => {
     const controller = app.controllers.vehicle_controller;
   
-     app.route('/api/v1/vehicles')
-       .get(controller.listVehicles)
-       .post(controller.saveVehicle);
+    app.route('/api/v1/vehicles')
+        .get(controller.listVehicles)
+        .post(controller.saveVehicle);
 
     app.route('/api/v1/vehicle/:vehicleId')
-      .delete(controller.removeVehicle)
-      .put(controller.updateVehicle);
-  }
+        .delete(controller.removeVehicle)
+        .put(controller.updateVehicle);
+}
